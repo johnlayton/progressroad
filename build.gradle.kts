0c7f73cf15a164a4d95cf734aa6e539fb769c7ac
john layton <johnstewartlayton@gmail.com>
2023-08-03 11:52:28 +1000

    add gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter("5.9.1")
+        }
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+application {
+    mainClass.set("org.progressroad.App")
+}
