2280184a9d8bdc69add7263c9fcb2a4ccb7a6e4c
john layton <johnstewartlayton@gmail.com>
2023-08-03 13:51:39 +1000

    add status and implement tests

--- src/main/java/org/progressroad/Trip.java
+++ src/main/java/org/progressroad/Trip.java
@@ -1,5 +1,12 @@
 package org.progressroad;
 
+import static org.progressroad.Trip.Status.COMPLETED;
+
 public record Trip(Tap on, Tap off) {
 
+    public Status status() {
+        return COMPLETED;
+    }
+
+    public enum Status {CANCELLED, COMPLETED, INCOMPLETE, INVALID}
 }
--- src/test/java/org/progressroad/TripTest.java
+++ src/test/java/org/progressroad/TripTest.java
@@ -1,22 +1,41 @@
 package org.progressroad;
 
 import org.junit.jupiter.api.Test;
+import org.progressroad.Tap.Type;
+
+import java.time.OffsetDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.progressroad.Tap.Type.ON;
+import static org.progressroad.Tap.Type.OFF;
+import static org.progressroad.Trip.Status.*;
 
 public class TripTest {
 
     @Test
     void shouldReportTripCompleteForTapOnAndTapOffAtDifferentStops() {
-
+        assertEquals(COMPLETED, new Trip(create(ON, "stopId1"), create(OFF, "stopId2")).status());
     }
 
     @Test
     void shouldReportTripCancelledForTapOnAndTapOffAtSameStops() {
-
+        assertEquals(CANCELLED, new Trip(create(ON, "stopId1"), create(OFF, "stopId1")).status());
     }
 
     @Test
     void shouldReportTripIncompleteForTapOnWithMissingTapOff() {
-
+        assertEquals(INCOMPLETE, new Trip(create(ON, "stopId1"), null).status());
     }
 
+    private static Tap create(final Type type, final String stopId) {
+        return new Tap(
+                1,
+                OffsetDateTime.now(),
+                type,
+                stopId,
+                "companyId",
+                "busId",
+                "pan"
+        );
+    }
 }
